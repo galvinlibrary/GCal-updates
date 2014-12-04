f23eccb448b358c88b6e658e9e9b5edf0de5b310
Christine McClure <camcclure@gmail.com>
2014-12-04 13:53:52 -0600

    created galvin batch script

--- batch/galvin_load_calendar.php
+++ batch/galvin_load_calendar.php
@@ -1,10 +1,8 @@
 <?php
-
-
+  require_once("../batchFunctions/batchFunctions.php");
+  
   $table='hours_gdata_galvin';
-  $Itable='hours_gdata_ncfst';
-  $ifshCal='iit.edu_5i0pd1jtpscfvfbebc1b2of5jc@group.calendar.google.com';
   $galvinCalendar='iit.edu_8l0d8qd4qtfn7skmgkiu55uv58%40group.calendar.google.com';
-  $sql = load_calendar_data(60, $ifshCal, $Itable);
-
+  load_calendar_data(60, $galvinCalendar, $table);
+  
 ?>
